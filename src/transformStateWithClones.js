3a091e5e5885010ba4d9f36c2481d269ff3b3fd6
tymchukm <103190723+tymchukm@users.noreply.github.com>
2025-03-26 19:46:31 +0200

    fix task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,18 +11,23 @@ function transformStateWithClones(state, actions) {
   const history = [];
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      currentState = {};
-    } else if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      currentState = { ...currentState };
+    const stateCopy = { ...currentState };
 
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        currentState = stateCopy;
     }
-    history.push(currentState);
+
+    history.push({ ...currentState });
   }
 
   return history;
